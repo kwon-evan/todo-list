dee34f39a77be1d5b5522d97839bbf8cbaaf0e7e
heonjin <koun012@naver.com>
2024-07-25 11:31:35 +0900

    feat: add create button action

--- web/src/lib/todo-utils.js
+++ web/src/lib/todo-utils.js
@@ -13,8 +13,22 @@ export async function getTodo(id) {
 }
 
 export async function createTodo(todo) {
+  if (typeof todo !== 'string' || todo.trim() === '') {
+    throw new Error('Todo must be a non-empty string');
+  }
+
   const endpoint = url + "/todos/";
-  return await axios.post(endpoint, todo).then((res) => res.data);
+  const payload = {
+    "value": todo.trim(),
+    "done": false
+  };
+
+  try {
+    const response = await axios.post(endpoint, payload);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 }
 
 export async function updateTodo(id, todo) {
